8229a01cdbe3aec97faf2bb37f7e9e3fe4f7c753
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-09-04 14:10:05 +0300

    Initial jytkycode

---
+++ jytkybasic code/jytkybasic code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace jytkybasic_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
