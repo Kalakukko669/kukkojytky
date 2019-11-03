bc4a81f5e7499b3f5d97bf4397699371fb69b1b6
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-11-03 22:06:52 +0200

    Made change program name

---
+++ Iftaskwhile 3.4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Iftaskwhile_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            int i = 0;
+            int Summa = 0;
+
+            Console.WriteLine("Ohjelma laskee lukua N yhteen");
+            Console.Write("syötä luku:");
+            N = int.Parse(Console.ReadLine());
+
+            if (N==0)
+            {
+                Console.WriteLine("Virheellinen syöte");
+            }
+            else
+            {
+                while (i != N) 
+                {
+                    if (i > 0)
+                    {
+                        i = i + 1;
+                        Summa = Summa + i;
+                    }
+                    else
+                    {
+                        i = i - 1;
+                        Summa = Summa + i;
+                    }
+                                          
+                }
+                Console.WriteLine("Vastaus" + Summa);
+            }
+            
+        }
+    }
+}
