a4d8574a1bc6d12f1e6b4c34c8971780a5d8b786
Kalakukko669 <aatu.savolainen@student.saimia.fi>
2019-11-04 09:08:56 +0200

    made project 4.3 ready

--- iftaskdowhilefor 4.1/iftaskdowhilefor 4.3/Program.cs
+++ iftaskdowhilefor 4.1/iftaskdowhilefor 4.3/Program.cs
@@ -8,8 +8,12 @@ namespace iftaskdowhilefor_4._3
         {
             Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua väliltä 0-50 ");
             Random rnd = new Random();
-            Console.WriteLine()
-            
+            int y = 0;
+            do
+            {
+                y++;
+                Console.WriteLine($"Rivi\t{y}:\t{rnd.Next(50)},{rnd.Next(50)},{rnd.Next(50)},{rnd.Next(50)},{rnd.Next(50)}");                
+            } while (y < 4);
         }
     }
 }
